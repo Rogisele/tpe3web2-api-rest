d2f36906538c36208f94fa7744f262b3f1ca7f27
BryanDanielTolosa <89491961+BryanDanielTolosa@users.noreply.github.com>
2025-11-04 21:33:57 -0300

    Terminado

--- router.php
+++ router.php
@@ -1,29 +1,32 @@
 <?php
 require_once './libs/router.php';
-require_once './app/Apicontrollers/chapters.api.controller.php';
-require_once './app/Apicontrollers/seasons.api.controller.php';
+require_once './app/Apicontrollers/chapter.api.controller.php';
+require_once './app/Apicontrollers/season.api.controller.php';
 require_once './app/Apicontrollers/user.api.controller.php';
 
 // crea el router
 $router = new Router();
 
+// definimos la tabla de ruteo
+#                  endpoint       verbo     controlador         método
 
-// define la tabla de ruteo
-    #              endpoint ,  verbo ,     controller  ,       metodo
-
-$router->addRoute('season', 'GET', 'SeasonController', 'getAll');
+// Temporadas
+$router->addRoute('season', 'GET', 'SeasonController', 'getAll'); // o 'listCategories'
 $router->addRoute('season', 'POST', 'SeasonController', 'addSeason');
 $router->addRoute('season/:id', 'PUT', 'SeasonController', 'updateSeason');
 $router->addRoute('season/:id', 'GET', 'SeasonController', 'listCategoriesById');
 $router->addRoute('season/:id', 'DELETE', 'SeasonController', 'deleteSeason');
 
-/*$router->addRoute('usuarios/token', 'GET', 'UserApiController',   'getToken');*/
+// Capítulos
+$router->addRoute('chapters', 'GET', 'ChapterController', 'getAll');
+$router->addRoute('chapter', 'POST', 'ChapterController', 'add');
+$router->addRoute('chapter/:id', 'GET', 'ChapterController', 'getById');
+$router->addRoute('chapter/:id', 'PUT', 'ChapterController', 'update');
+$router->addRoute('chapter/:id', 'DELETE', 'ChapterController', 'delete');
 
-$router->addRoute('chapters', 'GET', 'ChaptersController', 'getAll');
-$router->addRoute('chapter', 'POST', 'ChaptersController', 'add');
-$router->addRoute('chapter/:id', 'GET', 'ChaptersController', 'getById');
-$router->addRoute('chapter/:id', 'PUT', 'ChaptersController', 'update');
-$router->addRoute('chapter/:id', 'DELETE', 'ChaptersController', 'delete');
+// Usuarios (opcional para login/token)
+$router->addRoute('login', 'POST', 'UserApiController', 'login'); // si implementás login
+
 // rutea
 $router->route($_GET['resource'], $_SERVER['REQUEST_METHOD']);
 ?>
