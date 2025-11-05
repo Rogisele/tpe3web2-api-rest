634f610846f1bd9d58c88fe0e5a68e16a3bd7b1c
BryanDanielTolosa <89491961+BryanDanielTolosa@users.noreply.github.com>
2025-11-04 21:36:59 -0300

    Completado

--- app/Apimodels/user.api.model.php
+++ app/Apimodels/user.api.model.php
@@ -1,19 +1,43 @@
 <?php
-require_once 'tpe3web2-api-rest/database/config.php';
+require_once './app/Apiviews/apiview.php';
+require_once './app/Apimodels/user.api.model.php';
+require_once './auth/jwt.php';
 
-class UserModel {
-    private $db;
+class UserApiController {
+    private $model;
+    private $view;
+    private $data;
 
     public function __construct() {
-        $this->db = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USER, DB_PASS);
+        $this->model = new UserModel();
+        $this->view = new ApiView();
+        $this->data = file_get_contents("php://input");
     }
- 
-    public function getUserByEmail($user) {    
-        $query = $this->db->prepare("SELECT * FROM user WHERE Usuario = ?");
-        $query->execute([$user]);
-    
-        $user = $query->fetch(PDO::FETCH_OBJ);
-    
-        return $user;
+
+    private function getData() {
+        return json_decode($this->data);
+    }
+
+    public function login() {
+        $data = $this->getData();
+
+        if (!isset($data->usuario) || !isset($data->contraseña)) {
+            return $this->view->response(["error" => "Faltan datos"], 400);
+        }
+
+        $user = $this->model->getUserByUsername($data->usuario);
+
+        if ($user && password_verify($data->contraseña, $user->contraseña)) {
+            $payload = [
+                "id" => $user->id,
+                "usuario" => $user->Usuario,
+                "exp" => time() + 3600 // Token válido por 1 hora
+            ];
+            $token = createJWT($payload);
+            return $this->view->response(["token" => $token], 200);
+        } else {
+            return $this->view->response(["error" => "Credenciales inválidas"], 401);
+        }
     }
 }
+?>
