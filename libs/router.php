4c6b9cef7acb5a1ef6878d3778b5ca8193ad7060
Ro Rodriguez <gisele.rodriguez@outlook.com>
2025-11-12 08:18:38 -0300

    Add files via upload

--- libs/router.php
+++ libs/router.php
@@ -10,44 +10,44 @@ class Route {
     private $method;
     private $params;
 
-    public function __construct($url, $verb, $controller, $method){
+    public function __construct($url, $verb, $controller, $method) {
         $this->url = $url;
         $this->verb = $verb;
         $this->controller = $controller;
         $this->method = $method;
         $this->params = [];
     }
+
     public function match($url, $verb) {
-        if($this->verb != $verb){
-            return false;
-        }
-        $partsURL = explode("/", trim($url,'/'));
-        $partsRoute = explode("/", trim($this->url,'/'));
-        if(count($partsRoute) != count($partsURL)){
-            return false;
-        }
+        if ($this->verb !== $verb) return false;
+
+        $partsURL = explode("/", trim($url, '/'));
+        $partsRoute = explode("/", trim($this->url, '/'));
+
+        if (count($partsRoute) !== count($partsURL)) return false;
+
         foreach ($partsRoute as $key => $part) {
-            if($part[0] != ":"){
-                if($part != $partsURL[$key])
-                return false;
-            } //es un parametro
-            else
-            $this->params[''.substr($part,1)] = $partsURL[$key];
+            if ($part[0] !== ":") {
+                if ($part !== $partsURL[$key]) return false;
+            } else {
+                $this->params[substr($part, 1)] = $partsURL[$key];
+            }
         }
+
         return true;
     }
-    public function run($request, $response){
-        $controller = $this->controller;  
-        $method = $this->method;
+
+    public function run($request, $response) {
         $request->params = (object) $this->params;
-       
+        $controller = $this->controller;
+        $method = $this->method;
+
         (new $controller())->$method($request, $response);
     }
 }
 
 class Router {
     private $routeTable = [];
-    private $middlewares = [];
     private $defaultRoute;
     private $request;
     private $response;
@@ -58,33 +58,26 @@ class Router {
         $this->response = new Response();
     }
 
-    public function route($url, $verb) {
-        foreach ($this->middlewares as $middleware) {
-            $middleware->run($this->request, $this->response);
-        }
-        //$ruta->url //no compila!
-        foreach ($this->routeTable as $route) {
-            if($route->match($url, $verb)){
-                //TODO: ejecutar el controller//ejecutar el controller
-                // pasarle los parametros
-                $route->run($this->request, $this->response);
-                return;
-            }
-        }
-        //Si ninguna ruta coincide con el pedido y se configuró ruta por defecto.
-    //     if ($this->defaultRoute != null)
-    //         $this->defaultRoute->run($this->request, $this->response);
-     }
-
-    public function addMiddleware($middleware) {
-        $this->middlewares[] = $middleware;
-    }
-    
-    public function addRoute ($url, $verb, $controller, $method) {
+    public function addRoute($url, $verb, $controller, $method) {
         $this->routeTable[] = new Route($url, $verb, $controller, $method);
     }
 
     public function setDefaultRoute($controller, $method) {
         $this->defaultRoute = new Route("", "", $controller, $method);
+    }
+
+    public function route($url, $verb) {
+        foreach ($this->routeTable as $route) {
+            if ($route->match($url, $verb)) {
+                $route->run($this->request, $this->response);
+                return;
+            }
+        }
+
+        if ($this->defaultRoute !== null) {
+            $this->defaultRoute->run($this->request, $this->response);
+        } else {
+            $this->response->send(["error" => "Ruta no encontrada"], 404);
+        }
     }
 }
